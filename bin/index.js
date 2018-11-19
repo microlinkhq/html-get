aeff0a42aa05c646faf60f785a0e8eae1782e0ee
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-11-19 01:17:39 +0100

    feat: add cli

---
+++ bin/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { URL } = require('url')
+const getHTML = require('..')
+
+getHTML(new URL(process.argv[2]))
+  .then(({ html }) => {
+    console.log(html)
+    process.exit(0)
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
