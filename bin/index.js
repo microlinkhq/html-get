ee411282c1fd63ae4b87609852c19d3f529f4907
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-02-19 17:10:22 +0100

    fix: cli url

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 const { URL } = require('url')
 const getHTML = require('..')
 
-getHTML(new URL(process.argv[2]))
+getHTML(new URL(process.argv[2]).toString())
   .then(({ html }) => {
     console.log(html)
     process.exit(0)
