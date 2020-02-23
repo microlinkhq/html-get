7aa80e5de99effa89da00bdb824f73e62055744a
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-02-23 12:06:20 +0100

    fix: linter

--- src/index.js
+++ src/index.js
@@ -90,11 +90,11 @@ const prerender = async (
 
   return isFetchResRejected
     ? {
-        headers: fetchDataProps.headers || {},
-        html: '',
-        url,
-        mode: 'prerender'
-      }
+      headers: fetchDataProps.headers || {},
+      html: '',
+      url,
+      mode: 'prerender'
+    }
     : fetchDataProps
 }
 
