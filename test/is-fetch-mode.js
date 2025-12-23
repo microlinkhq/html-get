8930220b745d3f8510c13d137a5e89585ac51aef
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-12-23 11:18:13 +0100

    chore: remove twitter

--- test/is-fetch-mode.js
+++ test/is-fetch-mode.js
@@ -10,7 +10,8 @@ test('true', t => {
       'https://www.abc.net.au/news/2023-06-14/idpwd-2023-calling-all-budding-storytellers-with-disability/102388090'
     )
   )
-  t.true(
-    isFetchMode('https://twitter.com/Kikobeats/status/1741205717636264436')
-  )
+})
+
+test('false', t => {
+  t.false(isFetchMode('https://x.com/Kikobeats/status/1741205717636264436'))
 })
--- test/mode.js
+++ test/mode.js
@@ -20,26 +20,12 @@ test('`{ prerender: false }`', async t => {
 })
 
 test("`{ prerender: 'auto' }`", async t => {
-  {
-    const url = 'https://google.com'
-    const { stats } = await getHTML(url, {
-      getBrowserless,
-      puppeteerOpts: { adblock: false }
-    })
-    t.is(stats.mode, 'fetch')
-  }
-  {
-    const url = 'https://twitter.com/Kikobeats/status/1741205717636264436'
-    const { html, stats } = await getHTML(url, {
-      headers: {
-        'user-agent': 'Slackbot 1.0 (+https://api.slack.com/robots)'
-      },
-      getBrowserless,
-      puppeteerOpts: { adblock: false }
-    })
-    t.true(html.includes('og:title'))
-    t.is(stats.mode, 'fetch')
-  }
+  const url = 'https://google.com'
+  const { stats } = await getHTML(url, {
+    getBrowserless,
+    puppeteerOpts: { adblock: false }
+  })
+  t.is(stats.mode, 'fetch')
 })
 
 test.skip('prerender error fallback into fetch mode', async t => {
